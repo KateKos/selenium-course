45b5e3d91df90b2737b3d0a60e7fede7edcbf5ff
Kate Kos <ekaterinakos33@gmail.com>
2016-12-11 19:58:39 +0300

    added check for matching product title (text) and prices(text) on main page and product page

--- ruby/tasks/task10.rb
+++ ruby/tasks/task10.rb
@@ -1,25 +1,43 @@
 require './helpers.rb'
 
 describe 'Products' do
-	before (:each) do
-		browser_launch()
-	end
+  before (:each) do
+    browser_launch()
+  end
 
-	it 'checks correct product' do
-		sign_in('admin', 'admin')
+  it 'checks correct product' do
+    sign_in('admin', 'admin')
     @driver.navigate.to 'http://localhost/litecart/en/'
     
-    #checking navigation to correct product page
-    element_text = @driver.find_elements(:xpath, "//*[@id='box-campaigns']/div/ul/li/a").first.attribute("title")
+    #checking navigation to correct product page, comparing title and prices
+    products = @driver.find_elements(:xpath, "//*[@id='box-campaigns']/div/ul/li/a").first
+    product_title = products.attribute("title")
+    regular_price = products.find_element(:xpath, "//s[@class='regular-price']").attribute("textContent")
+    campaign_price = products.find_element(:xpath, "//strong[@class='campaign-price']").attribute("textContent")
+
     @driver.find_elements(:xpath, "//*[@id='box-campaigns']/div/ul/li").first.click
     page_header = @driver.find_element(:tag_name, "h1").attribute("textContent")
+    regular_inner_price = @driver.find_element(:xpath, "//s[@class='regular-price']").attribute("textContent")
+    campaign_inner_price = @driver.find_element(:xpath, "//strong[@class='campaign-price']").attribute("textContent")
     
-    if element_text == page_header
+    if product_title == page_header
       print "Product is correct"
     else
       print "Wrong product!"
     end
 
-		stop_browser()
-	end
+    if regular_price == regular_inner_price
+      print "Product's regular price is correct"
+    else
+      print "Product's regular price is wrong"
+    end
+
+    if campaign_price == campaign_inner_price
+      print "Product's campain price is correct"
+    else
+      print "Product's campain price is wrong"
+    end
+
+    stop_browser()
+  end
 end
